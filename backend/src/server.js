ef2f518a47e703527b47ae48cebb127ef700e375
kidusw <kidusworkneh555@gmail.com>
2025-07-05 10:15:27 +0300

    notif added and arcjet

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,16 @@ import {clerkMiddleware} from "@clerk/express";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from './routes/notification.route.js';
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get('/',(req,res)=>{
     res.send("hello from server");
@@ -20,6 +24,7 @@ app.get('/',(req,res)=>{
 app.use('/api/users',userRoutes);
 app.use('/api/posts',postRoutes);
 app.use('/api/comments',commentRoutes);
+app.use('/api/notificaions',notificationRoutes);
 
 // error handling middleware
 app.use((err,req,res)=>{
