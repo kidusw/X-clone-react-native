1f4ccb85b136ea2e3dd3754db4777b591139443e
kidusw <kidusworkneh555@gmail.com>
2025-07-05 21:28:21 +0300

    auth implemented

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,11 +1,11 @@
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
 
-export default function Index() {
+const HomeScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
-      </Text>
+    <View>
+      <Text>index</Text>
     </View>
   );
-}
+};
+
+export default HomeScreen;
